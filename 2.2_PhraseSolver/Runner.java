dc837e457fee284af122faf16b0366abcccc36a6
Rayan Dziri <rayan.dziri@crpusd.org>
2026-01-06 09:20:41 -0800

    TODOS done on Runner

--- 2.2_PhraseSolver/Runner.java
+++ 2.2_PhraseSolver/Runner.java
@@ -1,21 +1,41 @@
+import java.util.Scanner;
+
 /*
  * Activity 2.2.2
- *
  * The runner for the PhraseSolverGame
  */
 public class Runner
 {
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
     System.out.println("\n========================\n");
-    //Nicholas T: Printing a welcome message to the game
     System.out.println("Welcome to the phrase solver game, find a friend and compete to find the phrase!");
 
+    Scanner sc = new Scanner(System.in);
+    boolean quit = false;
+    boolean useDefaultFile = true;
 
-    //TODO: Add a game loop: Keep looping, querying user if they want to quit; end loop if so
-    //TODO: Switch between phrases.txt and phrases_OG.txt on each iteration of the loop.
-    PhraseSolver p = new PhraseSolver(); 
-    p.play();
+    while (!quit)
+    {
+      PhraseSolver game;
+
+      // Switch between phrase files each round
+      if (useDefaultFile)
+        game = new PhraseSolver("phrases.txt");
+      else
+        game = new PhraseSolver("phrases_OG.txt");
+
+      useDefaultFile = !useDefaultFile;
+      game.play();
+
+      System.out.print("Do you want to quit? (y/n): ");
+      String response = sc.nextLine().trim().toLowerCase();
+
+      if (response.equals("y"))
+        quit = true;
+    }
+
+    sc.close();
+    System.out.println("\nThanks for playing!");
   }
-
-} 
+}
