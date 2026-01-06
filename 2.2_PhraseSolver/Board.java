c7ec063636a3226a8a7d09362193331af4968ede
Pianocraft1YT <lucca.chen@crpusd.org>
2026-01-06 09:15:45 -0800

    revert change that nuked a todo that was done

--- 2.2_PhraseSolver/Board.java
+++ 2.2_PhraseSolver/Board.java
@@ -33,6 +33,10 @@ public class Board {
   }
 
   // TODO: Add Javadoc explaining intent of this method
+  /** 
+   * This method sets the point value of the current letter if guessed correctly, assigning it to currentLetterValue
+  */
+
   public void setLetterValue() {
     int randomInt = (int) ((Math.random() * 10) + 1) * 100;
     currentLetterValue = randomInt;
